932c13f1ff7b63581a31637f5dde738a03b6f120
Htet Aung Shine <h@shine.rocks>
2018-02-21 20:58:56 +0800

    Replaced screen and overlay with Adobe's code

--- src/lib.rs
+++ src/lib.rs
@@ -32,8 +32,8 @@ fn apply_1977(img: &RgbaImage) -> RgbaImage {
     let contrasted = imageops::contrast(img, 10.0);
     let brightened = rustaops::brighten_by_percent(&contrasted, 10.0);
     let saturated = rustaops::saturate(&brightened, 30.0);
-    let foreground = rustaops::pre_multiply(&saturated);
-    let background = rustaops::fill_with_channels(width, height, &[243,106,188,76]);
+    let background = rustaops::pre_multiply(&saturated);
+    let foreground = rustaops::fill_with_channels(width, height, &[243,106,188,76]);
     let out = rustaops::blend_screen(&foreground, &background);
     out
 }
@@ -41,8 +41,8 @@ fn apply_1977(img: &RgbaImage) -> RgbaImage {
 fn apply_gingham(img: &RgbaImage) -> RgbaImage {
     let (width, height) = img.dimensions();
     let brightened = rustaops::brighten_by_percent(img, 5.0);
-    let foreground = imageops::huerotate(&brightened, -10);
-    let background = rustaops::fill_with_channels(width, height, &[230, 230, 230, 255]);
+    let background = imageops::huerotate(&brightened, -10);
+    let foreground = rustaops::fill_with_channels(width, height, &[230, 230, 230, 255]);
     let out = rustaops::blend_soft_light(&foreground, &background);
     out
 }
--- src/rustaops/blend.rs
+++ src/rustaops/blend.rs
@@ -8,12 +8,12 @@ pub fn compute_final_alpha(fg: &[u8; 4], bg: &[u8; 4]) -> u8 {
     (final_alpha * 255.0) as u8
 }
 
+// ((uint8)(255 - (((255 - A) * (255 - B)) >> 8)))
 #[allow(dead_code)]
 pub fn blend_screen(x1: u8, x2: u8) -> u8 {
-    let f1 = x1 as f32 / 255.0;
-    let f2 = x2 as f32 / 255.0;
-    let v = 1.0 - (1.0 - f1) * (1.0 - f2);
-    (v * 255.0) as u8
+    let x1: u16 = x1 as u16;
+    let x2: u16 = x2 as u16;
+    (255 - ((255 - x1).wrapping_mul(255 - x2)>>8)) as u8
 }
 
 #[allow(dead_code)]
@@ -85,15 +85,13 @@ pub fn blend_overlay(x1: u8, x2: u8) -> u8 {
 
 #[allow(dead_code)]
 pub fn blend_soft_light(x1: u8, x2: u8) -> u8 {
-    let f1 = x1 as f32 / 255.0;
-    let f2 = x2 as f32 / 255.0;
-    let v;
-    if f2 >= 0.5 {
-        v = (2.0 * f1 * (1.0 - f2)) + (f1.sqrt() * (2.0 * f2 - 1.0));
+    let f2 = x2 as f32;
+    let shifted = (x1>>1) as f32;
+    if x2 < 128 {
+        ((2.0*((shifted)+64.0))*(f2/255.0)) as u8
     } else {
-        v = (2.0 * f1 * f2) + (f1 * f1 * (1.0 - 2.0 * f2));
+        (255.0-(2.0*(255.0-((shifted)+64.0))*(255.0-f2)/255.0)) as u8
     }
-    (v * 255.0) as u8
 }
 
 #[allow(dead_code)]
