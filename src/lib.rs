a414801e29165d960a52b3650a983116825bdf20
Htet Aung Shine <h@shine.rocks>
2018-02-23 17:11:35 +0800

    Removed redundant vectors

--- src/lib.rs
+++ src/lib.rs
@@ -6,29 +6,10 @@ mod rustaops;
 use filters::{FilterType};
 use filters::FilterType::*;
 
-pub fn available_filters<'a>() -> Vec<&'a str> {
-    vec![
-        "1977", "aden", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson",
-        "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville",
-        "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden"
-    ]
-}
-
-pub fn validate_filter_type(filter: &str) -> Result<FilterType, &'static str> {
-    let filters_strings = vec![
-        "1977", "aden", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson",
-        "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville",
-        "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden"
-    ];
-    let filters = vec![
-        NineTeenSeventySeven, Aden, Brannan, Brooklyn, Clarendon, Earlybird, Gingham, Hudson,
-        Inkwell, Kelvin, Lark, Lofi, Maven, Mayfair, Moon, Nashville, Reyes, Rise, Slumber, Stinson,
-        Toaster, Valencia, Walden
-    ];
-
-    let search_result = filters_strings.iter().enumerate().find(|f| &filter == f.1);
+pub fn validate_filter_type(filter: &str, filter_strings: &Vec<&str>, filter_types: &Vec<FilterType>) -> Result<FilterType, &'static str> {
+    let search_result = filter_strings.iter().enumerate().find(|f| &filter == f.1);
     match search_result {
-        Some((i,_)) => Ok(filters[i].clone()),
+        Some((i,_)) => Ok(filter_types[i].clone()),
         None => Err("Invalid filter type")
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,22 @@ extern crate rustagram;
 #[macro_use]
 extern crate clap;
 
+use rustagram::filters::FilterType::*;
 use std::process;
 
 fn main() {
-    let filter_arg = &format!("Filter name: {}", rustagram::available_filters().join(", "));
+    let filter_strings = vec![
+        "1977", "aden", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson",
+        "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville",
+        "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden"
+    ];
+    let filters = vec![
+        NineTeenSeventySeven, Aden, Brannan, Brooklyn, Clarendon, Earlybird, Gingham, Hudson,
+        Inkwell, Kelvin, Lark, Lofi, Maven, Mayfair, Moon, Nashville, Reyes, Rise, Slumber, Stinson,
+        Toaster, Valencia, Walden
+    ];
+
+    let filter_arg = &format!("Filter name: {}", filter_strings.join(", "));
     let matches = clap_app!(myapp =>
         (version: "1.0")
         (author: "Htet Aung Shine <h@shine.rocks>")
@@ -20,7 +32,7 @@ fn main() {
     let input = matches.value_of("INPUT").unwrap();
     let filter = matches.value_of("FILTER").unwrap();
 
-    let filter_type = match rustagram::validate_filter_type(&filter) {
+    let filter_type = match rustagram::validate_filter_type(filter, &filter_strings, &filters) {
         Ok(item) => item,
         Err(msg) => {
             eprintln!("{}", msg);
